3849124aaf7b8ecb5c5cc65ab1bcf6b109e2a445
TBA5854 <adhavan5030@gmail.com>
2024-12-01 23:12:32 +0530

    day1

---
+++ day1/sol_part_2.py
@@ -0,0 +1,12 @@
+with open('req.txt','r') as f:
+	a=f.readlines()
+	c=len(a)
+l1=[int(i.split()[0]) for i in a]
+l2=[int(i.split()[1]) for i in a]
+#l1.sort()
+#l2.sort()
+l3=[]
+for i in range(c):
+	print(l1[i],l2[i])
+	l3.append(l1[i]*l2.count(l1[i]))
+print(sum(l3))
